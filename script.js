15664e996e03100a2553d747cbcfa16c8ace6744
Ernest <zyperis14@gmail.com>
2025-08-18 15:40:25 +0300

    WIp

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ const addDOM = document.querySelector('.add');
 const notesDOM = document.querySelector('.notes')
 
 addDOM.addEventListener('click', () => {
-    // noteDOM.classList.add('active');
-    // console.log(notesDOM);
     notesDOM.insertAdjacentHTML('afterbegin', `<div class="note"><textarea name="note" id="note" placeholder="Empty note"></textarea></div>`);
 });
