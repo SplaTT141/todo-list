e41a2d645c34207c6f6b9831f208cc4967f88ae2
Ernest <zyperis14@gmail.com>
2025-08-19 21:19:42 +0300

    wip

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const addDOM = document.querySelector('.add');
-const notesDOM = document.querySelector('.notes')
+const notesDOM = document.querySelector('.notes');
+const noteDOM = document.getElementsByClassName('note');
 
 addDOM.addEventListener('click', () => {
-    notesDOM.insertAdjacentHTML('afterbegin', `<div class="note"><textarea name="note" id="note" placeholder="Empty note"></textarea></div>`);
+    notesDOM.insertAdjacentHTML('afterbegin', `
+        <div class="note">
+            <textarea name="note" id="note" placeholder="Empty note"></textarea>
+            <div class="remove">
+                <button class="confirm" type="submit">Confirm</button>
+                <buttom class="cancel" type="submit">Cancel</button>
+            </div>
+        </div>
+        `);
+
+    for (const note of noteDOM) {
+        note.addEventListener('dblclick', () => {
+            note.classList.add('visible');
+        })
+    }
 });
+
